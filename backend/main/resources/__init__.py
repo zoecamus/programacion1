063a53267e54bcb52344de5d10966eacadfca7f3
pilarmachinea <m.machinea@alumno.um.edu.ar>
2025-04-07 22:48:35 -0300

    Segundo intento tp2

---
+++ backend/main/resources/__init__.py
@@ -0,0 +1,10 @@
+from .pollo import Users as UsersResource
+from .pollo import User as UserResource
+from .pollo import Notificaciones as NotificacionesResource
+from .pollo import Valoracion as ValoracionResource
+from .pollo import Products as ProductsResource
+from .pollo import Product as ProductResource
+from .pollo import Pedidos as PedidosResource
+from .pollo import Pedido as PedidoResource
+from .pollo import LogIn as LogInResource
+from .pollo import LogOut as LogOutResource
