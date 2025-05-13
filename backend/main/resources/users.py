72b7df2e03ef285e006605ca9710bbb57fcf3d0e
Maria Eva Modarelli <modarellimariaeva@gmail.com>
2025-05-13 15:49:41 -0300

    cambios paginación

--- backend/main/resources/users.py
+++ backend/main/resources/users.py
@@ -1,16 +1,52 @@
 from flask import request, jsonify
 from flask_restful import Resource
-from main.models import Usuariomodel
+from main.models import Usuariomodel, Pedidomodel
 from main import db
+from sqlalchemy import desc, func
 
 USUARIOS = {}
 
 class Users(Resource):
     def get(self):
-        usuarios = db.session.query(Usuariomodel).all()
-        return jsonify ([usuario.to_json() for usuario in usuarios])
+        page = 1
+        per_page = 10
+        usuarios = db.session.query(Usuariomodel)
+        if request.args.get('page'):
+            page = int(request.args.get('page'))
+        if request.args.get('per_page'):
+            per_page = int(request.args.get('per_page'))
+
+
+        if request.args.get('Pedidos'):
+            usuarios = usuarios.outerjoin(Usuariomodel.usuario_pedido).group_by(Usuariomodel.id_usuario).having(func.count(Pedidomodel.id_pedido) >= int(request.args.get('Pedidos')))
+
+        #Filtro por apellido
+        if request.args.get('apellido'):
+            usuarios = usuarios.filter(Usuariomodel.apellido.like("%"+request.args.get('apellido')+"%"))
+
+        #Filtro por rol
+        if request.args.get('rol'):
+            usuarios = usuarios.filter(Usuariomodel.rol.like("%"+request.args.get('rol')+"%"))
+
+        #Ordeno por apellido
+        if request.args.get('sortby_apellido'):
+            usuarios = usuarios.order_by(desc(Usuariomodel.apellido))
+            
+
+        usuarios = usuarios.paginate(page=page, per_page=per_page, error_out=False)
+        return jsonify ({'usuarios': [usuario.to_json() for usuario in usuarios],
+                        'total': usuarios.total,
+                        'pages': usuarios.pages,
+                        'page': usuarios.page})
     
     def post(self):
+        pedidos_ids = request.get_json().get('pedidos')
         usuario = Usuariomodel.from_json(request.get_json())
+
+        if pedidos_ids:
+            pedidos = Pedidomodel.query.filter(Pedidomodel.id_pedido.in_(pedidos_ids)).all()
+            usuario.usuario_pedido.extend(pedidos)
+
         db.session.add(usuario)
         db.session.commit()
+        return usuario.to_json(), 201
